bfc7369fe28d30881e2ed69b132ab1a11e89bb6e
SMSCodes07 <48341791+SMSCodes07@users.noreply.github.com>
2020-04-06 20:11:28 -0400

    Login Commit
    En este commit se creo el formulario de inicio de sesion, con una opcion para recueprar la contraseña de la cuenta del usuario. Se hicieron cambios en el archivo del index.html

--- js/adminAuth.js
+++ js/adminAuth.js
@@ -1,4 +1,4 @@
-// Funcion para regirar a un nuevo usuario
+// Funcion para registrar a un nuevo usuario
 async function createAdminUser() {
     //Extrayendo datos del formulario
     const userName = document.getElementById('userName').value;
@@ -61,4 +61,51 @@ async function createAdminUser() {
     });
     // En caso de problemas durante el registro, se ejecutara este bloque de codigo
 }
-// Funcion para regirar a un nuevo usuario
+// Funcion para registrar a un nuevo usuario
+// Funcion para iniciar la sesion de un usuario
+async function loginAdminUser() {
+    //Extrayendo datos del formulario
+    const userEmail = document.getElementById('userEmail').value;
+    const userPassword = document.getElementById('userPassword').value;
+    //Extrayendo datos del formulario
+    // Iniciando la sesion del usuario
+    await firebase.auth().signInWithEmailAndPassword(userEmail, userPassword)
+    // Iniciando la sesion del usuario
+    // En caso de un login exitoso, se ejecutara este codigo
+    .then(() => {
+        // Extrayendo displayName
+        const displayName = firebase.auth().currentUser.displayName;
+        // Extrayendo displayName
+        // Redireccionando al usuario
+        alert('Bienvenid@ ' + displayName);
+        location.href="../../components/adminDashboard.html"; 
+        // Redireccionando al usuario
+    })
+    // En caso de un login exitoso, se ejecutara este codigo
+    // En caso de un error, se ejecutara el siguente bloque de codigo
+    .catch((error) => {
+        const errorCodes = error.code;
+        switch (errorCodes) {
+            case 'auth/invalid-email':
+              alert('La direccón de correo introducida es invalida');
+              break;
+            case 'auth/user-disabled':
+              alert('Estimado usuario, la cuenta vinculada a la dirección de correo introducida ha sido inhabilitada');
+              break;
+            case 'auth/user-not-found':
+              alert('No existe cuenta alguna vinculada a la dirección de correo introducida');
+              break;
+            case 'auth/wrong-password':
+              alert('Contraseña incorrecta');
+              break;
+          }
+    });
+    // En caso de un error, se ejecutara el siguente bloque de codigo
+}
+// Funcion para iniciar la sesion de un usuario
+// Funcion para recueprar la contraseña del usuario
+function sendPasswordLink() {
+    const emailToRecover = prompt('Introduzca la dirección de correo electrónico vinculada a su cuenta, y enviaremos un enlace de recuperación');
+    firebase.auth().sendPasswordResetEmail(emailToRecover);
+}
+// Funcion para recueprar la contraseña del usuario
